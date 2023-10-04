8bb1fbceb10e7fe8578b7185d061561af31e053f
Ben Hybert <53020786+Hybes@users.noreply.github.com>
2023-10-04 11:31:46 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -13,7 +13,7 @@
       <UNotifications />
       <div class="text-center mt-8 mb-14">
         <UButton class="my-2" color="red" variant="outline" @click="resetConfig()">Logout</UButton>
-        <p class="text-sm my-2 opacity-60 hover:opacity-80"><a href="https://github.com/dualtonecomms" target="_blank">Built by Dualtone</a></p>
+        <p class="text-sm my-2 opacity-60 hover:opacity-80"><a href="https://connectdorset.com" target="_blank">Built by Connect</a></p>
       </div>
     </div>
   </div>
